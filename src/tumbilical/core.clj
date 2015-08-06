cb436e4e890f221a8dd6cc316132c4b7902e666e
John Jacobsen <john@mail.npxdesigns.com>
2015-08-06 08:31:07 -0500

    Add avatar method.

--- src/tumbilical/core.clj
+++ src/tumbilical/core.clj
@@ -9,31 +9,52 @@
 (def ^:private tumblr-prefix "https://api.tumblr.com/v2/blog/")
 
 
-(defn info [blogname api-key]
+(defn avatar
+  "
+  (count (avatar \"johnjacobsenart.tumblr.com\" 16))
+  ;;=>
+  3183
+  (count (avatar \"johnjacobsenart.tumblr.com\" 512))
+  ;;=>
+  34668
+  (count (avatar \"johnjacobsenart.tumblr.com\"))
+  ;;=>
+  4331
+  "
+  [blogname & [size]]
+  {:pre [(or (nil? size) (#{16 24 30 40 48 64 96 128 512} size))]}
+  (->> (format "%s%s/avatar%s"
+               tumblr-prefix
+               blogname
+               (if size (str "/" size) ""))
+       clj-http.client/get
+       :body))
+
+
+(defn info
+  "
+  (info \"johnjacobsenart.tumblr.com\" (:tumblr-api-key env))
+  ;;=>
+  {:meta {:status 200, :msg \"OK\"},
+   :response
+   {:blog
+    {:description
+     \"Paintings, drawings, and various musings by John Jacobsen....\",
+     :updated 1438570197,
+     :name \"johnjacobsenart\",
+     :is_nsfw false,
+     :ask_page_title \"Ask me anything\",
+     :title \"John Jacobsen\",
+     :ask_anon false,
+     :share_likes false,
+     :url \"http://johnjacobsenart.tumblr.com/\",
+     :posts 32,
+     :ask false}}}
+  "
+  [blogname api-key]
   (->> api-key
        (format "%s%s/info?api_key=%s" tumblr-prefix blogname)
        clj-http.client/get
        :body
        parse-string
        clojure.walk/keywordize-keys))
-
-
-;; (info "johnjacobsenart.tumblr.com" (:tumblr-api-key env))
-
-;; ;;=>
-;; {:meta {:status 200, :msg "OK"},
-;;  :response
-;;  {:blog
-;;   {:description
-;;    "Paintings, drawings, and various musings by John Jacobsen. 100%  GMO-free or your money back.",
-;;    :updated 1438570197,
-;;    :name "johnjacobsenart",
-;;    :is_nsfw false,
-;;    :ask_page_title "Ask me anything",
-;;    :title "John Jacobsen",
-;;    :ask_anon false,
-;;    :share_likes false,
-;;    :url "http://johnjacobsenart.tumblr.com/",
-;;    :posts 32,
-;;    :ask false}}}
-
